aee45376b1b673e9c05a2c73e5e01bb0973ab7c0
JucaHV <133710434+JucaHV@users.noreply.github.com>
2023-05-21 21:19:58 -0600

    Commit inicial

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+// Este será el JavaScript en donde se pondrán todas las
+// funciones a utilizar en todos los htmls de este 
+// la página web
+
+// Para ir a la página 1 //
+function pagU1(){
+	window.location.href="unidad1.html";
+}
+// Para ir a la página de inicio //
+function pagInicio(){
+	window.location.href="index.html";
+}
+// Para ir a la página 2 //
+function pagU2(){
+	window.location.href="unidad2.html";
+}
+// Para ir a la página 3 //
+function pagU3(){
+	window.location.href="unidad3.html";
+}
+// Para ir a la página 4 //
+function pagU4(){
+	window.location.href="unidad4.html";
+}
+// Para ir a la página 5 //
+function pagPra(){
+	window.location.href="practicas.html";
+}
